b7490bd5acada0622424b13cb5c264cf0e16db12
GILGAMESH <xb011231@student.reading.ac.uk>
2025-03-18 17:17:01 +0000

    Add project files.

---
+++ BlockchainAssignment/Block.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockchainAssignment
+{
+    class Block
+    {
+        /** Block Properties */
+        DateTime timestamp;  // time block was created
+        int index; // the current position of the block in blockchain
+        String hash, prevHash; // the current hash and previous hash of block
+
+
+        /* Genesis Block Constructor */ // Code for creating the first block without any inputs 
+        public Block()
+        {
+            this.timestamp = DateTime.Now; // current time of block creation
+            this.index = 0; // set value to 0 as its the first block
+            this.prevHash = String.Empty; // No prior block hash as first block
+            this.hash = CreateHash(); // creates its hash
+        }
+        /* Block Constructors */ // creation of our blocks
+        public Block(int index, String hash) 
+        { 
+            this.timestamp = DateTime.Now; // get the current date/time
+            this.index = index + 1; // we incrment the index by 1 as its the next block in our chain
+            this.prevHash = hash; // creating block from previous hash
+            this.hash = CreateHash();
+        }
+
+
+        // Overloaded block constructor accepting a Block object
+        public Block(Block prevBlock) // creating block from a prev block
+        {
+            this.timestamp = DateTime.Now;
+            this.index = prevBlock.index + 1;
+            this.prevHash = prevBlock.hash;
+            this.hash = CreateHash();
+        }
+
+
+        /* Generate a blocks hash using the SHA256 algorithm */ 
+        public String CreateHash()
+        {
+            SHA256 hasher = SHA256Managed.Create(); 
+
+            String input = index.ToString() + timestamp.ToString() + prevHash; 
+            Byte[] hashByte = hasher.ComputeHash(Encoding.UTF8.GetBytes(input)); 
+
+            String hash = string.Empty; 
+
+            foreach (byte x in hashByte) 
+                hash += String.Format("{0:x2}", x); 
+
+            return hash;
+        }
+
+
+        /* Return a human-readable representation of a block */
+        public override string ToString()
+        {
+            return
+                "Index: " + index.ToString() + "\tTimestamp: " + timestamp.ToString() +
+                "\nHash: " + hash +
+                "\nPrevious Hash: " + prevHash;
+        }
+    }
+}
---
+++ BlockchainAssignment/BlockchainApp.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace BlockchainAssignment
+{
+    partial class BlockchainApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.printBlock = new System.Windows.Forms.Button();
+            this.blockIndex = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.richTextBox1.Location = new System.Drawing.Point(18, 18);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(984, 481);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // printBlock
+            // 
+            this.printBlock.Location = new System.Drawing.Point(18, 508);
+            this.printBlock.Name = "printBlock";
+            this.printBlock.Size = new System.Drawing.Size(70, 61);
+            this.printBlock.TabIndex = 1;
+            this.printBlock.Text = "Print Block";
+            this.printBlock.UseVisualStyleBackColor = true;
+            this.printBlock.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // blockIndex
+            // 
+            this.blockIndex.Location = new System.Drawing.Point(110, 525);
+            this.blockIndex.Name = "blockIndex";
+            this.blockIndex.Size = new System.Drawing.Size(62, 26);
+            this.blockIndex.TabIndex = 2;
+            // 
+            // BlockchainApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1022, 740);
+            this.Controls.Add(this.blockIndex);
+            this.Controls.Add(this.printBlock);
+            this.Controls.Add(this.richTextBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "BlockchainApp";
+            this.Text = "Blockchain App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button printBlock;
+        private System.Windows.Forms.TextBox blockIndex;
+    }
+}
+
